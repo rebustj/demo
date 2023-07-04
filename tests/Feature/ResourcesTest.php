137a94d029bb3d3a8f76a26a6beb412c3280fb72
Ash Monsh <wh7r.com@gmail.com>
2023-07-05 01:22:34 +0300

    wip

--- tests/Feature/ResourcesTest.php
+++ tests/Feature/ResourcesTest.php
@@ -8,29 +8,8 @@ use LaraZeus\Bolt\Filament\Resources\ResponseResource;
 use function Pest\Laravel\get;
 use function Pest\Livewire\livewire;
 
-it('can render Category List', function () {
-    get(CategoryResource::getUrl())
-        ->assertSuccessful();
-});
-
-/*it('can list posts', function () {
-    $forms = \LaraZeus\Bolt\Models\Form::factory()->count(10)->create();
-
-    livewire(ListForms::class)
-        ->assertCanSeeTableRecords($forms);
-});*/
-
-it('can render Collection List', function () {
-    get(CollectionResource::getUrl())
-        ->assertSuccessful();
-});
-
-it('can render Form List', function () {
-    get(FormResource::getUrl())
-        ->assertSuccessful();
-});
-
-it('can render Response List', function () {
-    get(ResponseResource::getUrl())
-        ->assertSuccessful();
+it('can not edit', function () {
+    get(\LaraZeus\Bolt\Filament\Resources\CategoryResource::getUrl('edit', [
+        'record' => \LaraZeus\Bolt\Models\Category::factory()->create(),
+    ]))->assertSuccessful();
 });
